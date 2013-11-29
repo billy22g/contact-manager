9febd415e18d253ce43b401cdf4b41bdf455ad58
Billy <navyosu@gmail.com>
2013-11-29 14:24:46 -0700

    Added validation to email address to ensure an email address must be present

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:email_address) do 
+    EmailAddress.new(address: "me@example.com")
+  end
+
+  it "is valid" do
+    expect(email_address).to be_valid
+  end
+
+  it "is invalid without an email address" do
+    email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
 end
