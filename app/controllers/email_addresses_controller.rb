ba77a5f6a7b84e926c40c7bc788322d34461457e
Billy <navyosu@gmail.com>
2013-11-30 15:48:44 -0700

    Added new and create methods for email controller

--- app/controllers/email_addresses_controller.rb
+++ app/controllers/email_addresses_controller.rb
@@ -1,2 +1,20 @@
 class EmailAddressesController < ApplicationController
+
+  def new
+    @email_address = EmailAddress.new(person_id: params[:person_id])
+  end
+
+  def create
+    @email_address = EmailAddress.new(email_params)
+    if @email_address.save
+      redirect_to @email_address.person
+      flash.notice = "This email was successfully added to #{@email_address.person.first_name}"
+    else
+      render action: 'new'
+    end
+  end
+
+  def email_params
+    params.require(:email_address).permit(:address, :person_id)
+  end
 end
