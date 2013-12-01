f204446af87769d3a720b53fdccdbc305da56328
Billy <navyosu@gmail.com>
2013-12-01 10:11:53 -0700

    Changed person_id to contact_id for phone numbers and altered spec to reflect change

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
+  has_many :phone_numbers
 
   validates :name, presence: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -11,4 +11,10 @@ describe Company do
     basedirectory.name = nil
     expect(basedirectory).not_to be_valid
   end
+
+  it "responds with its phone numbers after they're created" do
+    phone_number = basedirectory.phone_numbers.build(number: "333-4444")
+    expect(phone_number.number).to eq('333-4444')
+  end
+
 end
