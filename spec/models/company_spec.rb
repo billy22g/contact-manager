58f4b65e24b9850dbf8d0e5e49b57deac6b9fa0f
Billy <navyosu@gmail.com>
2013-12-01 09:53:13 -0700

    Added validation that company must have a name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Company do
-  
+  let(:basedirectory) { Company.create(name: "BaseDirectory.com") }
+
+  it "is valid" do
+    expect(basedirectory).to be_valid
+  end
+
+  it "is invalid without a name" do 
+    basedirectory.name = nil
+    expect(basedirectory).not_to be_valid
+  end
 end
