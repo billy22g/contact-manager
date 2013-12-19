764a35857bd0106fdedd80192f45a85ee2eb3784
Billy <navyosu@gmail.com>
2013-12-19 08:49:35 -0700

    Created branch table for branches

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201165751) do
+ActiveRecord::Schema.define(version: 20131219012335) do
+
+  create_table "branches", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "companies", force: true do |t|
     t.string   "name"
