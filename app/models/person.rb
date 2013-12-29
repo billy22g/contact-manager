0f8adbf12f327e833a5d16ed61498decf6618914
Billy <navyosu@gmail.com>
2013-12-28 19:59:59 -0700

    Added contact relationship for person and company relating to their email addresses

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :phone_numbers, as: :contact
-  has_many :email_addresses
+  has_many :email_addresses, as: :contact
   belongs_to :branch
   
   validates :first_name, :last_name, presence: true
