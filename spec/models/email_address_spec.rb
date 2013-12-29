fb81a14b6878a3715b2456ed07cf2803a3c8ffcb
Billy <navyosu@gmail.com>
2013-12-28 19:52:22 -0700

    Changed email addresses to belong to a contact and not a person or company

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe EmailAddress do
 
   let(:email_address) do 
-    EmailAddress.new(address: "me@example.com", person_id: 1)
+    EmailAddress.new(address: "me@example.com", contact_id: 1, contact_type: 'Person')
   end
 
   it "is valid" do
@@ -15,8 +15,8 @@ describe EmailAddress do
     expect(email_address).not_to be_valid
   end
 
-  it "is invalid without being associated with a person" do 
-    email_address.person_id = nil
+  it "is invalid without being associated with a contact" do 
+    email_address.contact_id = nil
     expect(email_address).not_to be_valid
   end
 end
