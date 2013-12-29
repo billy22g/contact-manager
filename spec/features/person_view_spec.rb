e07f179106310ed3951010a274ecd0f4532f2064
Billy <navyosu@gmail.com>
2013-12-28 20:09:38 -0700

    Added emails to person view spec

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -9,6 +9,8 @@ describe 'the person view', type: :feature do
   before(:each) do
     person.phone_numbers.create(number: "555-1234")
     person.phone_numbers.create(number: "555-5678")
+    person.email_addresses.create(address: "billy@basedirectory.com")
+    person.email_addresses.create(address: "anthony@basedirectory.com")
     visit person_path(person)
   end
 
@@ -32,6 +34,28 @@ describe 'the person view', type: :feature do
     page.click_on('Create Phone number')
     expect(current_path).to eq(person_path(person))
     expect(page).to have_content('555-8888')
+  end
+
+  it 'shows the email addresses' do
+    person.email_addresses.each do |email|
+      expect(page).to have_content(email.address)
+    end
+  end
+
+  it 'has a link to add a new email address' do
+    expect(page).to have_link('Add Email Address', href: new_email_address_path(contact_id: person.id, contact_type: 'Person'))
+  end
+
+  it 'has a link to delete a email address' do 
+    expect(page).to have_link('Delete')
+  end
+
+  it 'adds a new email address' do 
+    page.click_on('Add Email Address')
+    page.fill_in('Email Address', with: 'billy@miltraveler.com')
+    page.click_on('Create Email Address')
+    expect(current_path).to eq(person_path(person))
+    expect(page).to have_content('billy@miltraveler.com')
   end
 
 end
